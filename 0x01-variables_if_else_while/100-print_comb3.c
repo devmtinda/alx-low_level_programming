f7b24adecc3607c584c30f9e22952193209c928d
David Kyalo <davidkymutinda@gmail.com>
2022-03-11 12:05:46 -0800

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (b = a+1; b <= 57; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a == 56 && b == 57)
+			{
+				putchar('\n');
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	/* your code goes there */
+	return (0);
+}
