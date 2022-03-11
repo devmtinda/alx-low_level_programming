3b00c0d332cbee502aea1e014bde707a0dd877db
David Kyalo <davidkymutinda@gmail.com>
2022-03-11 11:15:55 -0800

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	/* your code goes there */
+
+	return (0);
+}
