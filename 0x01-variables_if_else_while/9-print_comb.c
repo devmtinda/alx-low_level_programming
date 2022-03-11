2fa1e6e614b521a91757607874917d1fd643fe21
David Kyalo <davidkymutinda@gmail.com>
2022-03-11 11:36:31 -0800

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 48; alpha <= 57; alpha++)
+	{
+		putchar(alpha);
+		if (alpha ==57)
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
