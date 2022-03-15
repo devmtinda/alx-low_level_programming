b060b8cae083149558967370d0a013043f8d3e32
David Kyalo <davidkymutinda@gmail.com>
2022-03-15 12:01:05 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,19 @@
 
 /**
  * print_last_digit - entry point
- * @n : number to check
+ * @a : number to check
  * Return:0 or 1 or -1
  *
  */
 int print_last_digit(int a)
 {
-	int res = a % 10;
+	int res;
+
+	if (a < 0)
+	{
+		n *= -1;
+	}
+	res = a % 10;
 
 	return (res);
-
 }
