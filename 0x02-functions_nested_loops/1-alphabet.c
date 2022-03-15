0ad419d406f77fb19025860d4f9155767ff53775
David Kyalo <davidkymutinda@gmail.com>
2022-03-15 09:02:23 -0700

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return=0
+ */
+int main(void)
+{
+	char alpha = 'a' ;
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
