dae962e92084049e8334355ad32b6eaca3d26b43
David Kyalo <davidkymutinda@gmail.com>
2022-03-15 11:55:15 -0700

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @n : number to check
+ * Return:0 or 1 or -1
+ *
+ */
+int print_last_digit(int a)
+{
+	int res = a % 10;
+
+	return (res);
+
+}
