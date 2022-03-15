66f3a7de81ab1266e62f2193f33ff4141f36adb6
David Kyalo <davidkymutinda@gmail.com>
2022-03-15 12:48:20 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,20 +15,21 @@ void jack_bauer(void)
 
 	while (a < 3)
 	{
-		_putchar(a + '0');
 
 		while (b < 4)
 		{
-			_putchar(b + '0');
-			_putchar(':');
 
 			while (c < 6)
 			{
-				_putchar(c + '0');
 
 				while (d < 10)
 				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
 					_putchar(d + '0');
+					_putchar('\n');
 					d++;
 				}
 				c++;
