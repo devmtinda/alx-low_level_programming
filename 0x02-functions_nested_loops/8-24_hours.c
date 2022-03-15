c3aaacbbac566d9691779f2ad47706b79a193422
David Kyalo <davidkymutinda@gmail.com>
2022-03-15 12:39:01 -0700

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *jack_bauer - entry point
+ * @n : number to check
+ * Return:0 or 1 or -1
+ *
+ */
+void jack_bauer(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+
+	while (a < 3)
+	{
+		_putchar(a + '0');
+
+		while (b < 4)
+		{
+			_putchar(b + '0');
+			_putchar(':');
+
+			while (c < 6)
+			{
+				_putchar(c + '0');
+
+				while (d < 10)
+				{
+					_putchar(d + '0');
+					d++
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+}
