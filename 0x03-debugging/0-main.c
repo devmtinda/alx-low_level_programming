47cef43b65a6e2c899126510ecb0a3e0d566fed5
David Kyalo <davidkymutinda@gmail.com>
2022-03-16 04:32:03 -0700

    Create 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - entry point
+ *
+ * Return:0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+
+	else if (i < 0)
+	{
+		printf("%i is negative\n", i);
+	}
+
+	else
+	{
+		printf("%i is zero\n", i);
+	}
+
+	return (0);
+}
