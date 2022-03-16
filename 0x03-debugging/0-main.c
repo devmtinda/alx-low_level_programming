22b2529a25ae8bb6404ed62584f736a92282bab8
David Kyalo <davidkymutinda@gmail.com>
2022-03-16 04:51:27 -0700

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- * main - entry point
+ * main - Test function for positive or negative
  *
- * Return:0
+ * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
 	i = 98;
+
 	positive_or_negative(i);
+
 	return (0);
 }
