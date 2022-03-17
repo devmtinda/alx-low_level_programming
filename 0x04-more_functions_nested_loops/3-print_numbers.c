65d9471e8ecc0285c8ea312d046a02043346acda
David Kyalo <davidkymutinda@gmail.com>
2022-03-17 11:19:10 -0700

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Return:void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+}
