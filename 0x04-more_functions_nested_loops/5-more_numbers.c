ce20a551eb10164569da9545a0d964c11a0dd758
David Kyalo <davidkymutinda@gmail.com>
2022-03-17 12:24:02 -0700

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - entry point
+ * Return:0
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j = 0;
+	int rem;
+
+	while (i < 10)
+	{
+		while (i < 15)
+		{
+			if (i > 9)
+			{
+				_putchar('1');
+				rem = i - 10;
+				_putchar(rem + '0');
+			}
+			i++;
+		}
+		_putchat('\n');
+		j++;
+	}
+}
