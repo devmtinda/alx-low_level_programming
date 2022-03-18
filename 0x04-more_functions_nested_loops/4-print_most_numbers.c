cb7acfe60e0f0fd1b8a5a2e9181a2a982ec7348b
David Kyalo <davidkymutinda@gmail.com>
2022-03-17 22:36:04 -0700

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 		{
 			continue;
-			i++;
 		}
 		_putchar('0' + i);
-		i++;
 	}
 	_putchar('\n');
 }
