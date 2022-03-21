8c4f7b201f158971738678000a2b3a730c72723c
David Kyalo <davidkymutinda@gmail.com>
2022-03-21 11:44:04 -0700

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ * @s: passed string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	for (i = 0; ; i++)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+	}
+	len = i;
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
