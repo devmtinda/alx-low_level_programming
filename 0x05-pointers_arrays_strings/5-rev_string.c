47c2fb9e7de6f0288484a83a6aca463c6c3cc768
David Kyalo <davidkymutinda@gmail.com>
2022-03-21 12:01:22 -0700

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - entry point
+ * @s: passed string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, len, j;
+
+	for (i = 0; ; i++)
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+	}
+	len = i;
+	j = 0;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		s[j] = s[i];
+		j++;
+	}
+}
