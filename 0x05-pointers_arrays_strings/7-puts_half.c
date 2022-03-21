58793f27cc581c01584a88c97e38b76ed2419dd5
David Kyalo <davidkymutinda@gmail.com>
2022-03-21 13:31:42 -0700

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * puts_half - entry point
+ * @str: pointer to string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, n, i, j;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	if (len % 2)
+	{
+		n = (len - 1) / 2;
+		j = len;
+
+		for (i = 1; i < n; i++)
+		{
+			j -= 1;
+		}
+
+		while (j < len)
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+
+	else
+	{
+		n = len / 2;
+
+		while (n < len)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
