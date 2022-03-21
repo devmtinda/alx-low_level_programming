dd3716d3027376a98741ae0a1a59617a1a5d721b
David Kyalo <davidkymutinda@gmail.com>
2022-03-21 13:56:22 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,11 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		_putchar(',');
-		_putchar(' ');
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
