6b5525f5b042992c99c52d246c5859640c4fbabd
David Kyalo <davidkymutinda@gmail.com>
2022-03-22 00:07:24 -0700

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,9 +7,25 @@
  */
 int main(void)
 {
-	int passwd;
+	int i, sum, n;
+	int pass[100];
 
-	srand(time(0));
-	passwd = rand() - RAND_MAX / 2;
-	return passwd;
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
