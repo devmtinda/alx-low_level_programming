2854aa16a874d2bc8eb8968d194886158b488ae6
David Kyalo <davidkymutinda@gmail.com>
2022-03-21 23:49:39 -0700

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int passwd;
+
+	srand(time(0));
+	passwd = rand() - RAND_MAX / 2;
+	return 0;
+}
