6644b93035da86d641271f9ab09b54e3a09966b0
David Kyalo <davidkymutinda@gmail.com>
2022-03-23 11:29:37 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,31 @@
 #include "main.h"
 /**
+ * to_upper - function
+ * cap_string - entry point
+ * @str: string
+ * @i: integer
+ * Return: char
+ */
+void to_upper(char *str, int i)
+
+{
+	char upper, lower;
+
+	if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+	{
+		upper = 'A';
+		for (lower = 'a'; lower <= 'z'; lower++)
+		{
+			if (str[i + 1] == lower)
+			{
+				str[i + 1] = upper;
+				break;
+			}
+			upper++;
+		}
+	}
+}
+/**
  * cap_string - entry point
  * @str: string
  * Return: char
@@ -7,7 +33,6 @@
 char *cap_string(char *str)
 {
 	int len, i, j;
-	char upper, lower;
 	char sep[] = ",\t;\n; .!?\"(){}";
 
 	for (len = 0; str[len] != '\0'; len++)
@@ -19,19 +44,8 @@ char *cap_string(char *str)
 		{
 			if (str[i] == sep[j])
 			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				{
-					upper = 'A';
-					for (lower = 'a'; lower <= 'z'; lower++)
-					{
-						if (str[i + 1] == lower)
-						{
-							str[i + 1] = upper;
-							break;
-						}
-						upper++;
-					}
-				}
+				to_upper(str, i);
+
 			}
 		}
 	}
