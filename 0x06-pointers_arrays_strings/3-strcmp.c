6eb3833845dde006ded3926b1133cbb8fd0a4c6e
David Kyalo <davidkymutinda@gmail.com>
2022-03-24 08:09:15 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,26 +7,21 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len, len2, cmp, i;
+	int len, cmp, i;
 
 	for (len = 0; s1[len] != '\0'; len++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 
 	for (i = 0; i <= len; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] > s2[i])
-				cmp = 15;
-			else if (s1[i] < s2[i])
-				cmp = -15;
+			cmp = s1[i] - s2[i];
 			break;
 		}
-		else if (i == len && s1[i] == s2[i] && s1[i + 1] == s2[i + 1])
+		else if (i == len)
 		{
-			cmp = 0;
+			cmp = s1[i] - s2[i];
 
 		}
 	}
