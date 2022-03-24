fdf44bbcf55603b020baef8eef204551de2bd052
David Kyalo <davidkymutinda@gmail.com>
2022-03-24 03:40:30 -0700

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' ; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		if (src[i] == src[n])
 		{
-			dest[i] = '\0';
 			break;
 		}
 		dest[i] = src[i];
