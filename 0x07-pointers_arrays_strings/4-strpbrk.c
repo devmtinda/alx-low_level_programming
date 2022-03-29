e3d35b999ff1ac029c948445ee3d565a0c52f565
David Kyalo <davidkymutinda@gmail.com>
2022-03-29 10:37:05 -0700

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strpbrk - entry point
+ * @s: char pointer
+ * @accept: char pointer
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *f = s;
+
+	for (i = 0; ; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				f = s + i;
+				break;
+			}
+		}
+		if (s[i] == accept[j])
+		{
+			break;
+		}
+		else if (s[i] == '\0')
+		{
+			f = '\0';
+			break;
+		}
+	}
+	return (f);
+}
