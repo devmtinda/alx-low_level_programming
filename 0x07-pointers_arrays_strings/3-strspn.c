18011824184f8824c525cc9137569b36fc3be30d
David Kyalo <davidkymutinda@gmail.com>
2022-03-29 10:21:40 -0700

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, n = 0;
 
-	for (i = 0; s[i] != ','; i++)
+	for (i = 0; s[i] != ',' && s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
