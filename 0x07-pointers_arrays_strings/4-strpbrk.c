e468c9168e08de9bc6ecf6fa05f8bcbd60e07c3f
David Kyalo <davidkymutinda@gmail.com>
2022-03-29 11:41:52 -0700

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,28 +8,18 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char *f = s;
+	char *f;
 
-	for (i = 0; ; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				f = s + i;
-				break;
+				f = &s[i];
+				return (f);
 			}
 		}
-		if (s[i] == accept[j])
-		{
-			break;
-		}
-		else if (s[i] == '\0')
-		{
-			f = '\0';
-			break;
-		}
 	}
-	s = f;
-	return (s);
+	return (0);
 }
