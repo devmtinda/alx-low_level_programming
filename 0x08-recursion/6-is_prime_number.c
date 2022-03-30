4ce322bdf5f0a9beb918a41e41b1bc4824b35dda
David Kyalo <davidkymutinda@gmail.com>
2022-03-30 11:34:26 -0700

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * is_prime_number - entry point
+ * @n: integer
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	int num;
+
+	if (n == 1 || n == -1)
+		num = 0;
+	
+	else if (n % 2 && n % 3 && n % 5)
+		num = 1;
+
+	else
+		num = 0;
+
+	return (num);
+}
