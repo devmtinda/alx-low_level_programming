eaa8c101a535665fcefc50cd489b82277135f03b
David Kyalo <davidkymutinda@gmail.com>
2022-03-29 23:20:01 -0700

    Create 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - entry point
+ * @s: pointer char
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	char *p;
+
+	if (s[0] == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		p = &s[1];
+		_puts_recursion(p);
+	}
+}
