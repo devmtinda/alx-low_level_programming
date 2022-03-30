93851c524dbb1c8f61bf4834046752592d3af676
David Kyalo <davidkymutinda@gmail.com>
2022-03-30 12:01:11 -0700

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * square - entry point
+ * @n: integer
+ * @val: integer
+ */
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	}
+	else if (val * val < n)
+	{
+		return (square(n, val + 1));
+	}
+	else
+		return (-1);
+}
+/**
+ * _sqrt_recursion -entry point
+ * @n: integer
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	return (square(n, 0));
+}
