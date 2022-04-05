dc6675163a1f95549d6e98354d57eda845355566
David Kyalo <davidkymutinda@gmail.com>
2022-04-05 11:40:23 -0700

    Create 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns pointer
+ * @str: string
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	p = malloc(len);
+
+	if (p == NULL)
+		return (NULL);
+
+	i = 0;
+
+	while (i < len)
+	{
+		p[i] = str[i];
+		i++;
+	}
+
+	return (p);
+}
