e2821d391eaf1727a82ff9519c43c17ad7adc948
David Kyalo <davidkymutinda@gmail.com>
2022-04-11 01:13:39 -0700

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
