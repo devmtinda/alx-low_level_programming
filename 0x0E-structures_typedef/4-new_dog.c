d8b9240ed50be4cb32015ac573ba638f5826a01f
David Kyalo <davidkymutinda@gmail.com>
2022-04-11 07:50:39 -0700

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * new_dog - creates a new dog
@@ -10,10 +11,38 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t dogs;
 	dog_t *my_dog = &dogs;
+	char *p1, *p2;
+	int len1, len2, i;
 
-	my_dog->name = name;
+	for (len1 = 0; name[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; owner[len2] != '\0'; len2++)
+		;
+
+	p1 = malloc(sizeof(char) * (len1 + 1));
+	if (p1 == NULL)
+		return (NULL);
+
+	p2 = malloc(sizeof(char) * (len2 + 1));
+	if (p2 == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < len1)
+	{
+		p1[i] = name[i];
+		i++;
+	}
+	i = 0;
+	while (i < len2)
+	{
+		p2[i] = name[i];
+		i++;
+	}
+
+	my_dog->name = p1;
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = p2;
 
 	return (my_dog);
 }
