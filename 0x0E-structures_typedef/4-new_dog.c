d32f5b85ec9694d8a5f04b42bda8b18c154e8691
David Kyalo <davidkymutinda@gmail.com>
2022-04-11 12:08:02 -0700

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t dogs;
 	dog_t *my_dog = &dogs;
-	int len1, len2;
-
-	for (len1 = 0; name[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; owner[len2] != '\0'; len2++)
-		;
-
-	my_dog = malloc(sizeof(char) * (len1 + len2 + 2));
-	if (my_dog == NULL)
-		return (NULL);
 
 	my_dog->name = name;
 	my_dog->owner = owner;
