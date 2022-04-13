fb7d19e640f0197a2b9bec8f766d802327653009
David Kyalo <davidkymutinda@gmail.com>
2022-04-13 07:32:12 -0700

    Create 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * int_index - searches for integer
  * @array: integer array
@@ -10,6 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i, res;
 
 	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
 		return (-1);
 
 	i = 0;
