0ba6509b7d84287a2c64dd181e98c0983c1b1ad8
David Kyalo <davidkymutinda@gmail.com>
2022-04-13 07:28:05 -0700

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+/**
+ * int_index - searches for integer
+ * @array: integer array
+ * @size: array length
+ * @cmp: pointer function
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res;
+
+	if (size <= 0)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		res = (*cmp)(array[i]);
+		if (res != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+
+	return (-1);
+}
