400a871108c7861174460fe78a9be6cda8371af3
David Kyalo <davidkymutinda@gmail.com>
2022-04-14 11:28:42 -0700

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument point
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, res;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (*op != '+' && *op != '-' && *op != '*' && *op != '/' && *op != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	res = (get_op_func(op))(num1, num2);
+	printf("%d\n", res);
+	return (0);
+}
