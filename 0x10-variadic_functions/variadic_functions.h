3c7263aa7fc1fd2c233bea4c1645dac27e77c1ff
David Kyalo <davidkymutinda@gmail.com>
2022-04-19 05:50:08 -0700

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,4 +3,13 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+/**
+ * struct format - structure
+ * @operate: string
+ */
+typedef struct format
+{
+char *operate;
+} form;
 #endif
