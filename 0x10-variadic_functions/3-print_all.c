b24c981375e0c5ae6e8f4651ee32c4640e57c3a0
David Kyalo <davidkymutinda@gmail.com>
2022-04-20 08:34:20 -0700

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
+ * print_comma - prints comma
+ * @format: string
+ * @i: integer
+ * Return: void
+ */
+void print_comma(const char * const format, int i)
+{
+	switch (format[i + 1])
+	{
+		case '\0':
+			break;
+		default:
+			printf(", ");
+			break;
+	}
+}
+/**
  * switch_statement - entry point
  * @i: integer
  * @format: reference string
@@ -21,22 +38,24 @@ void switch_statement(int i, const char * const format, va_list ap)
 		case 'c':
 			charc = va_arg(ap, int);
 			printf("%c", charc);
+			print_comma(format, i);
 			break;
 		case 'i':
 			integ = va_arg(ap, int);
 			printf("%d", integ);
+			print_comma(format, i);
 			break;
 		case 'f':
 			f = va_arg(ap, double);
 			printf("%f", f);
+			print_comma(format, i);
 			break;
 		case 's':
 			p = va_arg(ap, char*);
 			if (p == NULL)
 				p = "(nil)";
 			printf("%s", p);
-			break;
-		default:
+			print_comma(format, i);
 			break;
 	}
 }
@@ -60,14 +79,6 @@ void print_all(const char * const format, ...)
 	while (format[i] != '\0')
 	{
 		switch_statement(i, format, ap);
-		switch (format[i + 1])
-		{
-			case '\0':
-				break;
-			default:
-				printf(", ");
-				break;
-		}
 		i++;
 	}
 	printf("\n");
