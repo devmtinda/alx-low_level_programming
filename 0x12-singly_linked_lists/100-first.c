d0ca48495e716d398a43e68ecdf79b018eadd7f3
David Kyalo <davidkymutinda@gmail.com>
2022-04-22 07:47:25 -0700

    Create 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * before - runs before main function
  */
-void __attribute__ (constructor) before()
+void __attribute__ ((constructor)) before()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
