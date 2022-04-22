fc395dc9e0d792c2321a561125c2436df1c92157
David Kyalo <davidkymutinda@gmail.com>
2022-04-22 06:31:30 -0700

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list
+ * @head: pointer to first node of list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	while (head->next != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+	free(head->str);
+	free(head);
+}
