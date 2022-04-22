194bf3e22ca189fc0f2a3b5f55359fe7f06e9395
David Kyalo <davidkymutinda@gmail.com>
2022-04-22 07:44:08 -0700

    Create 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: Always 0
+ * before - runs before main function
  */
-void __attribute__ ((constructor)) premain()
+void __attribute__ ((constructor)) before()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
