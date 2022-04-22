55b3da98911f8432525acdfee3efd97e57f265f6
David Kyalo <davidkymutinda@gmail.com>
2022-04-22 07:23:56 -0700

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
