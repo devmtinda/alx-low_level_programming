f64b1592b432e3635f0eeefcaef85768d8de5dec
David Kyalo <davidkymutinda@gmail.com>
2022-04-22 07:40:53 -0700

    Create 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,7 @@
  * main - entry point
  * Return: Always 0
  */
-int main(void)
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-	return (0);
 }
