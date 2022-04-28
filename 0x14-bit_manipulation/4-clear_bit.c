ad6c7bfe7b73b75f91a437ac3b4ca2f7f9b99957
David Kyalo <davidkymutinda@gmail.com>
2022-04-28 12:06:00 -0700

    Create 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * power - returns power
+ * @i: integer
+ * Return: unsigned int
+ */
+unsigned int power(unsigned int i)
+{
+	unsigned int p = 1;
+
+	while (i > 0)
+	{
+		p = p * 2;
+		i--;
+	}
+	return (p);
+}
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: number
+ * @index: integer
+ * Return: int
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int sum = 0, i = 0, num;
+
+	while (1)
+	{
+		if (index == i)
+			num = 0;
+
+		else
+			num = *n - 2 * (*n >> 1);
+
+		sum = sum + (num * power(i));
+		*n = *n >> 1;
+		i++;
+		if (*n == 0 && i > index)
+		{
+			*n = sum;
+			return (1);
+		}
+	}
+	return (-1);
+}
