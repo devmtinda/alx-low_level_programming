b2c92045d8d4ddb9884cca1680cfff8734eda686
David Kyalo <96857630+davidmutinda@users.noreply.github.com>
2022-05-01 13:30:00 +0300

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,4 @@
 #include "main.h"
-#include <limits.h>
-/**
- * power - returns the value of 2 power the argument
- * @i: unsigned integer
- * Return: unsigned int
- */
-unsigned int power(unsigned int i)
-{
-	unsigned int p = 1;
-
-	while (i > 0)
-	{
-		p = p * 2;
-		i--;
-	}
-	return (p);
-}
 /**
  * get_bit - returns value of a bit
  * @n: number to be converted to binary
@@ -26,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0, num;
 
-	if (power(index) > INT_MAX)
+	if (index >= 31)
 		return (-1);
 	while (n)
 	{
