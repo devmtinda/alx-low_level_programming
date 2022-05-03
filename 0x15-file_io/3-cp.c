dfae399c1bf48f7c62dae4c8364deb19febe250c
David Kyalo <davidkymutinda@gmail.com>
2022-05-03 12:00:55 -0700

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ * print_error2 - prints error
+ * @file2: second file
+ * Return: void
+ */
+void print_error2(char *file2)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file2);
+	exit(99);
+}
+/**
+ * print_error1 - prints error
+ * @file1: first file
+ * Return: void
+ */
+void print_error1(char *file1)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file1);
+	exit(98);
+}
+/**
  * cp_file - copies from file1 to file2
  * @file1: first file
  * @file2: second file
@@ -13,30 +33,18 @@ void cp_file(char *file1, char *file2)
 
 	fd1 = open(file1, O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file1);
-		exit(98);
-	}
+		print_error1(file1);
 	fd2 = open(file2, O_WRONLY | O_TRUNC | O_CREAT, 00664);
 	if (fd2 == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file2);
-		exit(99);
-	}
+		print_error2(file2);
 	buf = malloc(sizeof(char) * 1024);
 	m = read(fd1, buf, 1024);
 	buf[m] = '\0';
 	if (m == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file1);
-		exit(98);
-	}
+		print_error1(file1);
 	n = write(fd2, buf, m);
 	if (n == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file2);
-		exit(99);
-	}
+		print_error2(file2);
 	p = close(fd1);
 	q = close(fd2);
 	if (p == -1)
@@ -47,7 +55,6 @@ void cp_file(char *file1, char *file2)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fd2);
 	}
-	free(buf);
 }
 /**
  * main - entry point
