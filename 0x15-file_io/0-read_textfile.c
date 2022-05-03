e1ca442c5ed0e7a105e85c6bd31f3f102d82b300
David Kyalo <davidkymutinda@gmail.com>
2022-05-03 04:57:48 -0700

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 
 	if (fd == -1)
 		return (0);
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buf[max] = '\0';
-	printf("%s", buf);
+	dprintf(STDOUT_FILENO, "%s", buf);
 
 	close(fd);
 	return (max);
