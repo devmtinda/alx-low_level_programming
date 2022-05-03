5a04a93d926cfe86a8b28a43b1ef3ce8c7ad5f6c
David Kyalo <davidkymutinda@gmail.com>
2022-05-03 10:12:43 -0700

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,20 +13,20 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		ret = write(fd, " ", 1);
-
-	else
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 		ret = write(fd, text_content, i);
 
-	if (ret == -1)
-		return (-1);
+		if (ret == -1)
+			return (-1);
+	}
+
 	close(fd);
 	return (1);
 }
