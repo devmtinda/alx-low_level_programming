af209dcfcad6f8a1fb8b1b8f174d8e26dd6776f0
devmutinda <davidkymutinda@gmail.com>
2022-06-30 17:34:47 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,6 +40,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (!strcmp(key, " "))
 		return (0);
+	else if (!key || !value)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 	{
