8f24e04d6225701303265fce3c45b766a15df2c1
devmutinda <davidkymutinda@gmail.com>
2022-06-30 15:06:09 +0000

    Create operations.c

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: sum of int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first int
+ * @b: second int
+ * Return: difference of int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: product of int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first int
+ * @b: second int
+ * Return: quotient of int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds modulus of a number
+ * @a: first int
+ * @b: second int
+ * Return: modulo of int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
