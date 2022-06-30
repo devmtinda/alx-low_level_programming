3a1779eff3eb5ac79d0b02ab53eb65b1f0967159
devmutinda <davidkymutinda@gmail.com>
2022-06-30 15:13:16 +0000

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
+	if (size == 0)
+		return (NULL);
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
