854fb58546700cd39db9553c21c0d0dc83b11eff
devmutinda <davidkymutinda@gmail.com>
2022-06-30 18:23:29 +0000

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *ptr;
 	unsigned long int index;
 
+	if (ht == NULL)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[index];
 	if (!ptr)
