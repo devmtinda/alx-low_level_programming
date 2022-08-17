692e1cc3a0c8972e4e261fa7be4d756068488b23
devmutinda <davidkymutinda@gmail.com>
2022-08-17 17:10:52 +0000

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module calculates the perimeter of an island
+"""
+
+
+def island_perimeter(grid):
+    per = 0
+
+    for i in range(len(grid)):
+        for p in range(len(grid[i])):
+            if grid[i][p] == 1:
+                per += 4
+                try:
+                    if grid[i - 1][p]:
+                        per -= 1
+                except Exception:
+                    per += 0
+                try:
+                    if grid[i][p - 1]:
+                        per -= 1
+                except Exception:
+                    per += 0
+                try:
+                    if grid[i][p + 1]:
+                        per -= 1
+                except Exception:
+                    per += 0
+                try:
+                    if grid[i + 1][p]:
+                        per -= 1
+                except Exception:
+                    per += 0
+    return per
